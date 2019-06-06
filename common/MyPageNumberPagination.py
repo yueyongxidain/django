17898528736f90914683d78c6b9b2f966d460602
yueyong <1397604589@qq.com>
2019-06-06 18:11:24 +0800

    add

--- common/MyPageNumberPagination.py
+++ common/MyPageNumberPagination.py
@@ -1,7 +1,7 @@
-from rest_framework.pagination import PageNumberPagination
+from common.pagination import PageNumberPagination
 
 class MyPageNumberPagination(PageNumberPagination):
-    page_size = 2
-    max_page_size = 5
+    page_size = 10
+    max_page_size = 100
     page_size_query_param = 'size'
     page_query_param = 'page'
